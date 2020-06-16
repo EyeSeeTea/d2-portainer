f83bd98ecac97cea3939ef1cf47f52e257dd0bad
Arnau Sanchez <pyarnau@gmail.com>
2020-06-16 12:22:37 +0200

    Set default url to /portainer

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const portainerUrl = process.env.REACT_APP_PORTAINER_URL || "http://localhost:9000";
+const portainerUrl = process.env.REACT_APP_PORTAINER_URL2 || "/portainer";
 ReactDOM.render(<App portainerUrl={portainerUrl} />, document.getElementById("root"));
